97013a8046af5726eb032717f7ffbff6be651cb2
dineshdevelops <dineshdevelops@gmail.com>
2022-10-20 13:21:42 +0530

    fix: login and signup form fix

---
+++ pages/authentication/login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from "../../styles/components/authentication/login.module.scss"
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login';
+
+const Login = () => {
+  return (
+    <div className={styles.login}>
+        <div className={styles.title}>Login</div>
+        <div className={styles.loginForm}>
+        <Avatar sx={{bgcolor:'#f50057',width: 66, height: 66}}>?</Avatar>
+        <TextField
+          required
+          id="outlined-required"
+          label="Email Id"
+        />
+        <TextField
+          id="outlined-password-input"
+          required
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      <Button variant="contained" sx={{bgcolor:'#f50057'}} endIcon={<LoginIcon />}>
+        Login
+      </Button>
+        </div>
+    </div>
+  )
+}
+
+export default Login
