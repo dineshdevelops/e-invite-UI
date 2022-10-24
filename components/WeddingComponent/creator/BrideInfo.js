f86710d545281b414491f3d4e8fd441171e7983e
dineshdevelops <dineshdevelops@gmail.com>
2022-10-24 21:00:45 +0530

    fix: Form State Management

--- components/WeddingComponent/creator/BrideInfo.js
+++ components/WeddingComponent/creator/BrideInfo.js
@@ -5,17 +5,36 @@ import nobita from "../../../public/assets/wedding/nobita.jpg"
 import Button from '@mui/material/Button';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import TextField from '@mui/material/TextField';
+import axios from "axios"
 
-const GroomInfo = ({weddingData,setWeddingData}) => {
+const BrideInfo = ({weddingData,setWeddingData}) => {
+  const [bridePhotoUrl,setBridePhotoUrl] = React.useState("");
   const handleChange=(event)=>{
     const {name,value}=event.target;
-    setWeddingData({...weddingData,brideDetails:{...weddingData[name],[name]:value}})
+    setWeddingData((prevInfo)=>{
+      const newBrideInfo = {...prevInfo};
+      newBrideInfo.brideDetails[name]=value;
+      return newBrideInfo
+    })
+  }
+  const uploadBridePhoto = async(e)=>{
+    console.log("Upload Bride Photo")
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("photo",file);
+    const res= await axios.post("http://localhost:8083/api/cloudinary/uploadImage",formData);
+    setBridePhotoUrl(res.data.secure_url);
+    weddingData.brideDetails.bridePhoto=bridePhotoUrl;
+    setWeddingData(weddingData);
+    console.log(bridePhotoUrl);
+    console.log(weddingData)
   }
   return (
     <div className={styles.brideInfo}>
         <Image src={nobita} width='150' height='150' alt="profileImage" />
-        <Button variant="contained" endIcon={<PhotoCameraIcon />} sx={{bgcolor:'#f50057'}} name="bridePhoto" onChange={handleChange} >
-            Upload Image
+        <Button variant="contained" component="label" endIcon={<PhotoCameraIcon />} name="bridePhoto" sx={{bgcolor:'#f50057'}}>
+          Upload Image
+          <input hidden accept="image/*" multiple type="file" name="bridePhoto" onChange={uploadBridePhoto} />
         </Button>
         <TextField id="outlined-basic" label="Bride Name" variant="outlined" name="brideName" onChange={handleChange} />
         <TextField
@@ -31,4 +50,4 @@ const GroomInfo = ({weddingData,setWeddingData}) => {
   )
 }
 
-export default GroomInfo
+export default BrideInfo
--- components/WeddingComponent/creator/GroomInfo.js
+++ components/WeddingComponent/creator/GroomInfo.js
@@ -5,18 +5,40 @@ import nobita from "../../../public/assets/wedding/nobita.jpg"
 import Button from '@mui/material/Button';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import TextField from '@mui/material/TextField';
+import axios from "axios"
 
 const GroomInfo = ({weddingData,setWeddingData}) => {
   // * Get name and value from Text Field
   const handleChange=(event)=>{
     const {name,value}=event.target;
-    setWeddingData({...weddingData,groomDetails:{...weddingData[name],[name]:value}})
+    setWeddingData((prevInfo)=>{
+      const newGroomInfo = {...prevInfo};
+      newGroomInfo.groomDetails[name]=value;
+      return newGroomInfo
+    })
+  }
+  const [groomPhotoUrl,setGroomPhotoUrl] = React.useState("");
+
+  const uploadGroomPhoto = async(e)=>{
+    console.log("Upload Groom Photo");
+    e.preventDefault();
+    const {name}=e.target;
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("photo",file);
+    const res= await axios.post("http://localhost:8083/api/cloudinary/uploadImage",formData);
+    setGroomPhotoUrl(res.data.secure_url);
+    weddingData.groomDetails.groomPhoto=groomPhotoUrl;
+    console.log(groomPhotoUrl)
+    setWeddingData(weddingData);
+    console.log(weddingData)
   }
   return (
     <div className={styles.groomInfo}>
         <Image src={nobita} width='150' height='150' alt="profileImage"  />
-        <Button variant="contained" endIcon={<PhotoCameraIcon />} sx={{bgcolor:'#f50057'}} name="groomPhoto" onChange={handleChange} >
-            Upload Image
+        <Button variant="contained" component="label" endIcon={<PhotoCameraIcon />} name="groomPhoto"  sx={{bgcolor:'#f50057'}}>
+          Upload Image
+          <input hidden accept="image/*" multiple type="file" name="groomPhoto" onChange={uploadGroomPhoto} />
         </Button>
         <TextField id="outlined-basic" label="Groom Name" variant="outlined" name='groomName' onChange={handleChange} />
         <TextField
