9cb72197e67685c0f997f5fd23aaeaa05135778e
dineshdevelops <dineshdevelops@gmail.com>
2022-11-10 17:59:29 +0530

    fix: share option functionality

--- components/WeddingComponent/Events.js
+++ components/WeddingComponent/Events.js
@@ -1,17 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
-import styles from "../../styles/components/weddingComponent/events.module.scss"
+import styles from "../../styles/components/weddingComponent/events.module.scss";
+import IconButton from '@mui/material/IconButton';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+
 const Events = ({eventProps}) => {
   // console.log("Events Props"+eventProps)
+  const handleCalendarClick = ()=>{
+    console.log("Handle Calendar Click")
+  }
   return (
         <div className={styles.events}>
-                <div className={styles.eventName}>Event Name:{eventProps.eventName}</div>
                 <div className={styles.eventDetails}>
+                    <div className={styles.eventName}>Event Name:{eventProps.eventName}</div>
                     <div className={styles.date}>Event Date:{eventProps.eventDate}</div>
                     <div className={styles.venue}>Event Venue:{eventProps.eventVenue}</div>
                     <div className={styles.time}>Event Time:{eventProps.eventTime}</div>
                     <div className={styles.map}>Map: <Link href='https://goo.gl/maps/Zvo686gFKGaAumke8'>Click Here</Link></div>
                 </div>
+                {/* //! Add to Calendar */}
+                {/* <div className={styles.addToCalendar} onClick={handleCalendarClick}  >
+                  <IconButton color="primary" aria-label="add to shopping cart">
+                    <EventAvailableIcon fontSize='large' />
+                  </IconButton>
+                  <p>Add to Calendar</p>
+                </div> */}
         </div>
   )
 }
